9c3e1efdc8f472d7aefdbf2d59ee2446024391bc
Bilal Ahmed <bilal.ahmed@wistech.biz>
2025-02-05 12:18:29 +0500

    migrations

--- database/migrations/2025_01_25_113046_create_members_table.php
+++ database/migrations/2025_01_25_113046_create_members_table.php
@@ -15,6 +15,29 @@ class CreateMembersTable extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->integer('subscriber_id');
+            $table->integer('member_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('member_title')->nullable();
+            $table->text('profile_photo')->nullable();
+            $table->string('reference_number')->nullable();
+            $table->text('company_name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('gender')->nullable();
+            $table->text('bill_to_name')->nullable();
+            $table->text('bill_to_company')->nullable();
+            $table->text('bill_to_address')->nullable();
+            $table->date('subscription_start_date')->nullable();
+            $table->date('subscription_end_date')->nullable();
+            $table->string('job_role')->nullable();
+            $table->enum('activated', ['Active', 'Inactive','N/A'])->default('N/A');
+            $table->enum('is_active', ['Active', 'Inactive','N/A'])->default('N/A');
+            $table->string('created_by')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('account_type')->nullable();
+            $table->string('sector_industries')->nullable();
+            $table->enum('membership_type', ['Individual', 'Business','Business Advance','N/A'])->default('N/A');
             $table->timestamps();
         });
     }
--- database/migrations/2025_01_25_113107_create_events_table.php
+++ database/migrations/2025_01_25_113107_create_events_table.php
@@ -15,6 +15,25 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->integer('subscriber_id');
+            $table->integer('event_id');
+            $table->text('event_title');
+            $table->string('name');
+            $table->string('email');
+            $table->string('event_user_type')->nullable();
+            $table->enum('event_member_going', ['Yes', 'No','N/A'])->default('N/A');
+            $table->enum('event_attended', ['Yes', 'No','N/A'])->default('N/A');
+            $table->string('reference_number')->nullable();
+            $table->text('company_name')->nullable();
+            $table->date('event_start_date')->nullable();
+            $table->date('event_end_date')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('job_role')->nullable();
+            $table->enum('status', ['Approved', 'Pending','Cancelled','Completed','N/A'])->default('N/A');
+            $table->string('event_primary_player')->nullable();
+            $table->string('sector_industries')->nullable();
+            $table->enum('membership_type', ['Individual', 'Business','Business Advance','N/A'])->default('N/A');
+            $table->enum('payment_status', ['Paid', 'Unpaid','Partial Paid','N/A'])->default('N/A');
             $table->timestamps();
         });
     }
